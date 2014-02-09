4a5a732e80e20ede173e4868e370f5d1725bb664
carribits <carribits@gmail.com>
2014-02-09 13:03:52 -0500

    added origin *

--- application/controllers/Trigger.php
+++ application/controllers/Trigger.php
@@ -20,28 +20,36 @@ class Trigger extends CI_Controller {
     }
 
     public function DailyMainMarketSummary() {
-      
+
         $date = $this->input->get('date');
         $summary = new DailyMainMarketSummary($date);
         $result = $summary->fetch();
 
-       ob_get_clean();
+        ob_get_clean();
 
-        $this->output
-                ->set_content_type('application/json')
-                ->set_output(json_encode($result));
+        $this->output->set_header("Access-Control-Allow-Methods:  GET, POST, OPTIONS");
+        $this->output->set_header("Access-Control-Allow-Origin: *");
+        $this->output->set_header("Access-Control-Expose-Headers: Access-Control-Allow-Origin");
+        $this->output->set_header("Access-Control-Allow-Credentials: true");
+        $this->output->set_content_type('application/json');
+        $this->output->set_output(json_encode($result));
     }
 
     public function DailyMainMarketDetail() {
-      
+
         $date = $this->input->get('date');
         $summary = new DailyMainMarketDetail($date);
         $result = $summary->fetch();
 
-       ob_get_clean();
+        ob_get_clean();
 
-        $this->output
-                ->set_content_type('application/json')
-                ->set_output(json_encode($result));
+
+        $this->output->set_header("Access-Control-Allow-Methods:  GET, POST, OPTIONS");
+        $this->output->set_header("Access-Control-Allow-Origin:  *");
+        $this->output->set_header("Access-Control-Expose-Headers: Access-Control-Allow-Origin");
+        $this->output->set_header("Access-Control-Allow-Credentials: true");
+        $this->output->set_header("Content-type: application/json");
+        $this->output->set_output(json_encode($result));
     }
+
 }
