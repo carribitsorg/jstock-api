b813c1e4a183acaaf439132afd4ef7e3778da7c3
zerobytes <markel.mairs@gmail.com>
2014-02-11 18:19:54 -0500

    included pchart

--- application/controllers/Trigger.php
+++ application/controllers/Trigger.php
@@ -1,6 +1,7 @@
 <?php
 
 //require_once('application/simple_html_dom.php'); 
+require_once('application/third_party/pchart/StockGraph.php');
 
 class Trigger extends CI_Controller {
 
@@ -50,6 +51,16 @@ class Trigger extends CI_Controller {
         $this->output->set_header("Access-Control-Allow-Credentials: true");
         $this->output->set_header("Content-type: application/json");
         $this->output->set_output(json_encode($result));
+    }
+
+    public function test() {
+        error_reporting(0);
+        ini_set('display_errors', 0);
+
+        $data = "79950.500;79865.290;79840.960;80227.600;79922.910;79619.380;79892.040;80173.720;79771.250;79636.630;79854.040;79567.500;80540.480;81315.470;82387.810;81436.230;81017.930;81192.730;78993.480;79435.220;79184.540;79193.510 Jan 13, 2014 - 79,950.50;Jan 14, 2014 - 79,865.29;Jan 15, 2014 - 79,840.96;Jan 16, 2014 - 80,227.60;Jan 17, 2014 - 79,922.91;Jan 20, 2014 - 79,619.38;Jan 21, 2014 - 79,892.04;Jan 22, 2014 - 80,173.72;Jan 23, 2014 - 79,771.25;Jan 24, 2014 - 79,636.63;Jan 27, 2014 - 79,854.04;Jan 28, 2014 - 79,567.50;Jan 29, 2014 - 80,540.48;Jan 30, 2014 - 81,315.47;Jan 31, 2014 - 82,387.81;Feb 03, 2014 - 81,436.23;Feb 04, 2014 - 81,017.93;Feb 05, 2014 - 81,192.73;Feb 06, 2014 - 78,993.48;Feb 07, 2014 - 79,435.22;Feb 10, 2014 - 79,184.54;Feb 11, 2014 - 79,193.51 13;14;15;16;17;20;21;22;23;24;27;28;29;30;31;Feb;4;5;6;7;10;11";
+        $graph = new StockGraph($data);
+        ob_get_clean();
+        $graph->output();
     }
 
 }
---
+++ application/third_party/pchart/StockGraph.php
@@ -0,0 +1,93 @@
+<?php
+
+/* pChart library inclusions */
+include("class/pData.class.php");
+include("class/pDraw.class.php");
+include("class/pImage.class.php");
+
+class StockGraph {
+
+    private $yScaleCount = 0;
+    private $data = '';
+    private $graphPhoto = null;
+
+    public function __construct($data) {
+        $this->data = $data;
+        $this->graphPhoto = $this->render();
+    }
+
+    public function render() {
+        /* Create and populate the pData object */
+        $MyData = new pData();
+        $points = $this->getPairs($this->data);
+        //var_dump($points);
+        $MyData->addPoints($points['y_axis'], "Stocks");
+        //$MyData->setSerieTicks("Probe 2",4); 
+        $MyData->setAxisName(0, "Volume");
+        $MyData->addPoints($points['x_axis_value'], "Labels");
+        $MyData->setSerieDescription("Labels", "Months");
+        $MyData->setAbscissa("Labels");
+
+        $MyData->setAxisDisplay(0, AXIS_FORMAT_CUSTOM, "YAxisFormat");
+
+        /* Create the pChart object */
+        $myPicture = new pImage(310, 180, $MyData);
+
+        /* Turn of Antialiasing */
+        $myPicture->Antialias = FALSE;
+        $serieSettings = array("R" => 255, "G" => 69, "B" => 0);
+        $MyData->setPalette("Stocks", $serieSettings);
+
+        $myPicture->setFontProperties(array("FontName" => "application/third_party/pchart/fonts/pf_arma_five.ttf", "FontSize" => 6));
+
+        /* Define the chart area */
+        $myPicture->setGraphArea(38, 0, 310, 170);
+
+        /* Draw the scale */
+        $scaleSettings = array("XMargin" => 10, "YMargin" => 10, "Floating" => TRUE, "GridR" => 255, "GridG" => 255, "GridB" => 255, "DrawSubTicks" => TRUE, "CycleBackground" => TRUE);
+        $myPicture->drawScale($scaleSettings);
+
+        $myPicture->Antialias = TRUE;
+
+        /* Draw the area chart */
+        $MyData->setSerieDrawable("Stocks", TRUE);
+        $myPicture->drawAreaChart();
+
+        /* Draw a line and a plot chart on top */
+        $MyData->setSerieDrawable("Stocks", TRUE);
+        $myPicture->setShadow(TRUE, array("X" => 1, "Y" => 1, "R" => 0, "G" => 0, "B" => 0, "Alpha" => 10));
+        $myPicture->drawLineChart();
+        $myPicture->drawPlotChart(array("PlotBorder" => TRUE, "PlotSize" => 1, "BorderSize" => 1, "Surrounding" => -60, "BorderAlpha" => 80));
+
+        return $myPicture;
+    }
+
+    public function output() {
+        /* Render the picture (choose the best way) */
+        $this->graphPhoto->autoOutput("pictures/example.Combo.area.lines.png");
+    }
+
+    function YAxisFormat($Value) {
+        return number_format(78000 + ($this->yScaleCount++ * 1200));
+    }
+
+    function getPairs($text) {
+        preg_match('/[A-Za-z]/', $text, $matches, PREG_OFFSET_CAPTURE);
+        $text = substr($text, $matches[0][1], strlen($text));
+        $pieces = explode(";", $text);
+        $pairs = array();
+
+        foreach ($pieces as $piece) {
+            if (preg_match('/[A-Za-z]+/', $piece) && strlen($piece) > 3) {
+                $val = explode(" - ", $piece);
+                $pairs['x_axis'][] = $val[0];
+                $pairs['x_axis_value'][] = substr($val[0], 4, 2);
+                $pairs['y_axis'][] = floatval(strtr($val[1], array(',' => '')));
+            }
+        }
+        return $pairs;
+    }
+
+}
+
+?>
